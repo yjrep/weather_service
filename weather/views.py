90b99edd8e5ee3c649f7b799bed4b7b04a171bf6
Yungjae Kim <me@yungjae.com>
2021-01-23 21:16:36 -0500

    added template page for each view

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,17 @@
-from django.http import HttpResponse
+from weather.models import ZipCode
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('index page')
+    context = {
+        'msg': 'Test message'
+    }
+
+    return render(request, 'index.html', context)
 
 def search_history(request, zip_code):
-    return HttpResponse('search page')
+    context = {
+        'zip_code': zip_code
+    }
+
+    return render(request, 'search_result.html', context)
